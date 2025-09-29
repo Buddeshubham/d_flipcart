0c3d19ed2fa6f30e98a7252297b8592abe8a5019
Buddeshubham <shubhambudde123@gmail.com>
2025-09-29 12:06:57 +0000

    another commit

--- api/server.cjs
+++ api/server.cjs
@@ -1,13 +1,4 @@
 const serverless = require("serverless-http");
-// server.js
-const app = require('../configs/server.cjs'); // or your main app file
+const app = require('../configs/server.cjs');
 
-// In serverless, export the handler
-if (process.env.IS_SERVERLESS) {
-  module.exports = app;
-} else {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Cloud server running on port ${PORT}`);
-  });
-}
+module.exports.handler = serverless(app);
