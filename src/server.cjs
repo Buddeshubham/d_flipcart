af39ac14c2144a3c0bb2c1a845a7f704fb4aaeca
Buddeshubham <shubhambudde123@gmail.com>
2025-10-01 14:29:52 +0000

    lisner is added

--- src/server.cjs
+++ src/server.cjs
@@ -1,4 +1,13 @@
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
+// const app = require("../configs/server.cjs");
+
+// module.exports = serverless(app); // ✅ default export
+
+
 const app = require("../configs/server.cjs");
 
-module.exports = serverless(app); // ✅ default export
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
